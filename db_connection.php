01e16bc9fe9e82c552f65e8c24a1a2894783dbc7
BagusHidayat21 <bagusreza21@gmail.com>
2025-06-01 18:39:58 +0700

    Update for db_connection

--- db_connection.php
+++ db_connection.php
@@ -1,11 +1,12 @@
 <?php
-$host = "localhost";         
-$username = "root";          
-$password = "";              
-$database = "e_learning";    
+$host = "http://sql12.freesqldatabase.com/";         
+$username = "sql12782454";          
+$password = "zu1ZaZxuf3";              
+$database = "sql12782454";    
+$port = 3306;
 
 // Membuat koneksi
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $database, $port);
 
 // Periksa koneksi
 if ($conn->connect_error) {
