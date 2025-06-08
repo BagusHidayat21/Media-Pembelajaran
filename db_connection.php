0cb70d15b03b9999fccb4acb3b66b0fe61794b4b
BagusHidayat21 <bagusreza21@gmail.com>
2025-06-08 20:39:19 +0700

    Update untraked files

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
-$host = "sql12.freesqldatabase.com";         
-$username = "sql12782454";          
-$password = "zu1ZaZxuf3";              
-$database = "sql12782454";    
-$port = 3306;
+$host = getenv('DB_HOST') ?: 'localhost';
+$username = getenv('DB_USERNAME') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: '';
+$database = getenv('DB_DATABASE') ?: '';
+$port = getenv('DB_PORT');
 
 // Membuat koneksi
 $conn = new mysqli($host, $username, $password, $database, $port);
@@ -12,5 +12,4 @@ $conn = new mysqli($host, $username, $password, $database, $port);
 if ($conn->connect_error) {
     die("Koneksi database gagal: " . $conn->connect_error);
 }
-
 ?>
